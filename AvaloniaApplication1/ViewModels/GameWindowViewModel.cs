b2f827c0a091acafdb37ed937cb11fab157ebed3
Marcelo_Medrano <marcelo.medrano0125@jala.university>
2026-02-13 19:51:49 -0500

    Cambios para actualizar el game engine y que verifique cada tick si el pacman esta tocando una pildora o energizante.

--- AvaloniaApplication1/ViewModels/GameWindowViewModel.cs
+++ AvaloniaApplication1/ViewModels/GameWindowViewModel.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
 using Avalonia.Threading;
 using AvaloniaApplication1.Engine;
 using AvaloniaApplication1.Models;
@@ -70,17 +72,30 @@ namespace AvaloniaApplication1.ViewModels
 
         public void StartGame()
         {
-            var pacmanSprite = _gameEngine.SpriteManager.LoadSprite("PacmanViews.png");
+            _gameEngine.Map = GameMap.CreateFromLayout(PacmanMaps.ClassicMap, tileSize: 16);
+
+            var pacmanSprite = _gameEngine.SpriteManager.LoadSprite("PacmanViewsFinal.png");
             if (pacmanSprite == null)
             {
                 Console.WriteLine("Failed Sprite");
                 return;
             }
 
-            _pacman = new Pacman(100, 100, pacmanSprite, spriteSize: 15);
-            _pacman.SetDirection(Pacman.PacmanDirection.Right);
+            var startPos = _gameEngine.Map.TileToWorld(1, 15);
+            //centrar el pacman en la celda que es 16x16 y el pacman 14x14 
+            //double centeredX = startPos.x + (16 -14) / 2; 
+            //double centeredY = startPos.y + (16 - 14) / 2;
+            _pacman = new Pacman(startPos.x + 1, startPos.y + 1, pacmanSprite, _gameEngine.Map, spriteSize: 16);
+            _pacman.SetNextDirection(Pacman.PacmanDirection.Right);
             _gameEngine.AddGameObject(_pacman);
-            GameObjects.Add(_pacman);
+
+            _gameEngine.CreatePellets(); //crear los puntos y power ups
+
+            GameObjects.Clear();
+            foreach (var obj in _gameEngine.GameObjects)
+            {
+                GameObjects.Add(obj);
+            }
         }
         
         public void HandleKeyPress(Avalonia.Input.Key key)
@@ -91,29 +106,29 @@ namespace AvaloniaApplication1.ViewModels
             {
                 case Avalonia.Input.Key.Up:
                 case Avalonia.Input.Key.W:
-                    _pacman.SetDirection(Pacman.PacmanDirection.Up);
+                    _pacman.SetNextDirection(Pacman.PacmanDirection.Up);
                     break;
                 case Avalonia.Input.Key.Down:
                 case Avalonia.Input.Key.S:
-                    _pacman.SetDirection(Pacman.PacmanDirection.Down);
+                    _pacman.SetNextDirection(Pacman.PacmanDirection.Down);
                     break;
                 case Avalonia.Input.Key.Left:
                 case Avalonia.Input.Key.A:
-                    _pacman.SetDirection(Pacman.PacmanDirection.Left);
+                    _pacman.SetNextDirection(Pacman.PacmanDirection.Left);
                     break;
                 case Avalonia.Input.Key.Right:
                 case Avalonia.Input.Key.D:
-                    _pacman.SetDirection(Pacman.PacmanDirection.Right);
+                    _pacman.SetNextDirection(Pacman.PacmanDirection.Right);
                     break;
             }
         }
+
 
         [RelayCommand]
         private void Back()
         {
             _main.GoBackToMenu();
         }
-
 
     }
 }
