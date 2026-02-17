9ecc83a0eb4d6d827a39119c849281e4165c231d
Marcelo_Medrano <marcelo.medrano0125@jala.university>
2026-02-17 10:08:55 -0500

    Implementación del ScoreWindowViewModel para tenerlo guardado todo ahi

--- AvaloniaApplication1/ViewModels/ScoreboardWindowViewModel.cs
+++ AvaloniaApplication1/ViewModels/ScoreboardWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AvaloniaApplication1.Engine;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace AvaloniaApplication1.ViewModels
 {
-    internal class ScoreboardWindowViewModel : ViewModelBase
+    public partial class ScoreboardWindowViewModel : ViewModelBase
     {
+        [ObservableProperty]
+        private int _score;
+
+        [ObservableProperty]
+        private string _elapsedTime = "0.0s";
+
+        [ObservableProperty]
+        private int _currentFPS;
+
+        public void UpdateFromEngine(GameEngine engine)
+        {
+            Score = engine.Score;
+            ElapsedTime = $"{engine.TotalTime:F1}s";
+            CurrentFPS = engine.CurrentFPS;
+        }
     }
 }
