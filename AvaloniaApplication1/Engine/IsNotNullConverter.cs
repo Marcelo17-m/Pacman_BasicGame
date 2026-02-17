41a67cd021bee1cceba0ab4259c778b252ebad19
Marcelo_Medrano <marcelo.medrano0125@jala.university>
2026-02-17 10:07:17 -0500

    Clases extras para poder hacer validaciones al momento se usar un ItemControl en el axaml facilitan el trabajo

---
+++ AvaloniaApplication1/Engine/IsNotNullConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaApplication1.Engine
+{
+    public class IsNotNullConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
